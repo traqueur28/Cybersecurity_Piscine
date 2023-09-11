188ede95c7d1f2fb6f871ea5fe00d3ed53ee585d
Jarod GOURLIN <jgourlin@made-f0Br7s3.clusters.42paris.fr>
2023-09-11 18:05:09 +0200

    debut scorpion

---
+++ Arachnida/Scorpion/inc/scorpion.hpp
@@ -0,0 +1,17 @@
+#ifndef SCORPION_HPP
+#define SCORPION_HPP
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <stdio.h>
+#include <ctype.h>
+
+typedef struct   s_data
+{
+    std::string url;
+    std::string path;
+    int depth;
+}   t_data;
+
+#endif
---
+++ Arachnida/Scorpion/src/scorpion.cpp
@@ -0,0 +1,9 @@
+#include "../inc/scorpion.hpp"
+#include <jpeglib.h> 
+
+int main(int argc, char **argv)
+{
+    (void)argc;
+    (void)argv;
+    return (0);
+}
